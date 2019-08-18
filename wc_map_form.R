5bb9b82a1dea9d02341d7057e02936646ee440ee
stuurps <barkerstu@gmail.com>
2019-08-18 18:55:08 +0100

    Add files via upload

--- wc_map_form.R
+++ wc_map_form.R
@@ -22,8 +22,9 @@ ui <- shinyUI(
     textInput("loni", labelMandatory("Longitude"), ""),
     dateInput("date1", labelMandatory("Date:")),
     sliderInput("rating", labelMandatory("Rating"), 0, 10, 5, ticks = FALSE),
+    textInput("free_text", labelMandatory("Write about your experience"), ""),
     textInput("email", labelMandatory("Email Address"),""),
-    checkboxInput("contact", labelMandatory("Contact me about new spots"), FALSE),
+    checkboxInput("contact", "Contact me about new spots", FALSE),
     actionButton("submit", "Submit")
     )
   )
@@ -34,11 +35,11 @@ server <- shinyServer(function(input, output, session) {
   observe({toggleState(id = "submit", condition = nchar(input$email) >= 3 & nchar(input$lati) > 8)
 
   
-  data <- fread("wc_loc")
+  data <- fread("wc_locv2.csv")
   ## Make your initial map
   output$map <- renderLeaflet({
     leaflet() %>%
-      addMarkers(lng = data$lng, lat = data$lat) %>%
+      addMarkers(lng = data$lng, lat = data$lat, label = (data$rating)) %>%
       setView(lng = -1.781637,
               lat = 53.581092,
               zoom = 6) %>%
@@ -64,14 +65,21 @@ server <- shinyServer(function(input, output, session) {
     tmp <- NULL
     tmp$lat <- as.numeric(input$lati)
     tmp$lng <- as.numeric(input$loni)
+    tmp$upload_date <- as.character(Sys.Date())
+    tmp$stay_date <- as.character(input$date1)
+    tmp$rating <- input$rating
+    tmp$free_text <- input$free_text
+    tmp$email <- input$email
+    tmp$opt_in <- input$contact
+    
     clat <- as.numeric(input$lati)
     clng <- as.numeric(input$loni)
     data <- rbind(data, tmp)
-    write.csv(data, "wc_loc", row.names = F)
+    write.csv(data, "wc_locv2.csv", row.names = F)
     print(data)
     #saveData(formData())
-    leafletProxy('map') %>%
-      addMarkers(lng = clng, lat = clat)
+    #leafletProxy('map') %>%
+    #  addMarkers(lng = clng, lat = clat)
     
   })
 })
